7e19057238c898f12014516697c9249ff3f1391a
Pruthvi-TW <pruthvi.pradhan@thoughtworks.com>
2025-06-06 10:55:49 +0530

    [AUTO-GENERATED] Add mock-uidai-service/src/main/java/com/mockuidai/enums/UidaiResponseStatus.java

---
+++ generated_code/ekyc_application_20250606_105532/mock-uidai-service/src/main/java/com/mockuidai/enums/UidaiResponseStatus.java
@@ -0,0 +1,12 @@
+```java
+package com.mockuidai.enums;
+
+public enum UidaiResponseStatus {
+    OTP_SENT,
+    OTP_GENERATION_FAILED,
+    VERIFIED,
+    INVALID_OTP,
+    EXPIRED_OTP,
+    UIDAI_SERVICE_FAILURE
+}
+```
