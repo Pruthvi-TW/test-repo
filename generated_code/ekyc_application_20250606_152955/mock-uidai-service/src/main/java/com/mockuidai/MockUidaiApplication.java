36703da00767e4f461ec9e5aa780f9b26974f619
Pruthvi-TW <pruthvi.pradhan@thoughtworks.com>
2025-06-06 15:30:50 +0530

    [AUTO-GENERATED] Add mock-uidai-service/src/main/java/com/mockuidai/MockUidaiApplication.java

---
+++ generated_code/ekyc_application_20250606_152955/mock-uidai-service/src/main/java/com/mockuidai/MockUidaiApplication.java
@@ -0,0 +1,25 @@
+package com.mockuidai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+public class MockUidaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockUidaiApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(10000);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
+}
